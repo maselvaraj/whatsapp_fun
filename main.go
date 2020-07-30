ca38eccf560c8630448507092d3d1894090a559f
maselvaraj <citizenmani@gmail.com>
2020-07-29 17:03:03 -0700

    go fmt

--- main.go
+++ main.go
@@ -32,12 +32,12 @@ func main() {
 		Text: "super.....",
 	}
 
-	msgId,err := wac.Send(msg)
+	msgId, err := wac.Send(msg)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error sending message: %v", err)
 		os.Exit(1)
 	} else {
-		fmt.Println("Message Sent -> ID : "+msgId)
+		fmt.Println("Message Sent -> ID : " + msgId)
 	}
 }
 
